d999b630ddd2e75b023a531a3c04305d1271b808
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-12-06 01:05:01 +0000

    Update ini.cpp

--- ini.cpp
+++ ini.cpp
@@ -1,6 +1,19 @@
 #include "ini.h"
 
+#ifndef __INI_PARSER_MAIN__
+#define __INI_PARSER_MAIN__
+#endif
+
+#define VALUES
+#define WINDOW
+#define DIRECTORIES
+
 int main(int argc, char** argv)
 {
+	INI_MAIN* INI;
+	INI->LOAD_INI("./");
+
+	WINDOW const char* WIN_ITERATION_ = INI->GET_VALUE("", "");
+	DIRECTORIES const char* DIRECT_ITERATION_ = INI->GET_VALUE("", "");
 	return 0;
 }
