63dbbc9c4da96541e131fba856b85567cdcba654
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-12-29 15:20:06 +0000

    Update ini.h

--- ini.h
+++ ini.h
@@ -1,58 +1,39 @@
 #pragma once
 
-#ifndef __INI_PARSER__
-#define __INI_PARSER__
+#ifndef INI_PARSER
+#define INI_PARSER
 #endif
 
-#include <iostream>
 #include <stdio.h>
-#include <stdlib.h>
-#include <Windows.h>
-
-#define NEW_LINE_CHAR "\r\n"
-#define NEW_LINE_CHAR "\r"
-#define NEW_LINE_CHAR "n"
 
 #if defined(_WIN32) || defined(_WIN64)
-#define INI_PARSER_MAIN
+#define __declspec(dllexport)
+#else
+#define __declspec(dllimport)
 #endif
 
-#define USE_STD_MAP 0
-#define EQUIVALENCE "=" 1
-#define NO_CHAR 2
 
-struct STD_MAP;
-struct STD_EQUIVALENCE;
-struct STD_NO_CHAR;
+#ifndef ALLOW_ML_RW
+#define ALLOW_ML_RW 1
+#endif
 
-typedef struct
+#ifndef ALLOW_BYTE_ORDER
+#define ALLOW_BYTE_ORDER 1
+#endif 
+
+#ifndef MAX_LINE
+#define MAX_LINE 200
+#endif
+
+typedef int* HANDLER(void* USER, const char* SECTION, const char* NAME, const char* VALUE);
+typedef char* READER(char* STRING, int NUM, void* STREAM);
+
+
+/* CROSS COMPATIBILITY WITH C LOGIC */
+
+#ifdef __cplusplus
+extern "C"
 {
-	const char* KEY;
-	const char* VALUE;
+}
 
-} INI_VALUES;
-
-typedef struct
-{
-	INI_VALUES* KEY_VALUE;
-	const char* SECTION_NAME;
-	LONG KEY_VALUE_COUNT;
-
-} INI_SECTION;
-
-typedef struct
-{
-	LONG SECTION_SCOUNT;
-	INI_SECTION* SECTIONS;
-
-} INI_DESC;
-
-typedef struct
-{
-	static void LOAD_INI(const char* FILE_PATH);
-	static void LOAD_INI_STRING(const char* STRING);
-	static const char* GET_VALUE(const char* SECTION, const char* KEY);
-	INI_DESC* PARSED_INFO();
-	INI_DESC* LOADED_FILE;
-
-} INI_MAIN;
+#endif
